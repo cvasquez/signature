2c5787c6a25fd91fd73819f767c3018990d3c27e
Chris Vasquez <christv@aweber.com>
2016-02-18 22:59:12 -0500

    Add some styles and structure

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+var firstName = '',
+    lastName = '',
+    position = '';
+
+updateSignature('#firstName', firstName, '.firstName');
+updateSignature('#lastName', lastName, '.lastName');
+updateSignature('#position', position, '.position');
+updateSignature('#phone', phone, '.phone');
+updateSignature('#website', website, '.website');
+updateSignature('#twitter', twitter, '.twitter');
+
+function updateSignature(elem, theVar, theField) {
+  $(elem).keyup(function() {
+    theVar = $(elem).val();
+
+    $(theField).html(theVar);
+  });
+}
